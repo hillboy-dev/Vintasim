6365403888421c0131eea6e6977b65714f4d117b
hillboy-dev <69072323+hillboy-dev@users.noreply.github.com>
2020-11-02 22:28:30 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$(".btn1").click(function(){
+ var audio = new Audio("audio/hello1.mp3");
+ audio.play()
+})
+
+$(".Done").click(function(){
+ var audio = new Audio("audio/celebrate1.mp3");
+ audio.play()
+})
